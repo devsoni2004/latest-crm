613c7d29ea128d444c32318bf5fade8dc63a6631
devsoni2004 <vipbrothers92@gmail.com>
2023-10-21 16:42:02 +0530

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devashish/b2b',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'aca113f666173f75b8ee300ac581dd6c9178014d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'devashish/b2b' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'aca113f666173f75b8ee300ac581dd6c9178014d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
